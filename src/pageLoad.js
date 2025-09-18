0a798e0f150024478068fa09f0bacd6ed1d0a191
Martyna Mika <67931479+MaMikaDev@users.noreply.github.com>
2025-09-18 13:53:25 +0100

    added pseudo logo (made by me) and made some css adjustments

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,3 +1,5 @@
+import Logo from "./pseudoLogo.png";
+
 function pageLoad(){
 
     // get the content div from the template.html
@@ -7,6 +9,10 @@ function pageLoad(){
     const title = document.createElement("h1");
     title.textContent = "Welcome to All Blue";
 
+    const logo = document.createElement("img");
+    logo.src = Logo;
+    logo.alt = "Pseudo Logo and image check really";
+
     // create extra div just for testing
     const tagline = document.createElement("p");
     tagline.textContent = '"Where the only thing worse than food is the service"';
@@ -14,6 +20,7 @@ function pageLoad(){
 
     // adding created title and tagline to the content div
     content.appendChild(title);
+    content.appendChild(logo);
     content.appendChild(tagline);
 
 }
