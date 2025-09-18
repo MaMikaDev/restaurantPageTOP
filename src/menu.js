6e32bda91521943b3ca50314bc2c7082d378711d
Martyna Mika <67931479+MaMikaDev@users.noreply.github.com>
2025-09-18 13:01:55 +0100

    corrected ticks to backticks

--- src/menu.js
+++ src/menu.js
@@ -8,20 +8,31 @@ function loadMenu(){
     const menu = document.createElement("h1");
     menu.textContent = "Our Current Menu";
 
-    // create extra div just for testing
-    const menuOne = document.createElement("p");
-    menuOne.textContent = "All the meat you can eat (and some vegetables)";
-    menuOne.style.padding = "20px";
+    // creating menu items
+    const menuItems = [
+        {name: "Seafood pasta", description: "with a bit of spice", price: "£18.50"},
+        {name: "All the meat you can eat", description: "and some vegetables", price: "£15"},
+        {name: "Marimo special", description: "rice and fish, less sugar content", price: "£20"},
+        {name: "Mikan parfait", description: "citrusy and light", price: "£10"},
+        {name: "Garden sandwiches", description: "fresh and floral", price: "£17.50"}
+    ]
 
-     const menuTwo = document.createElement("p");
-    menuTwo.textContent = "Marimo special (rice with fish basically)";
-    menuTwo.style.padding = "20px";
+    const menuList = document.createElement("ul");
+    menuList.style.padding = "2rem";
+
+    menuItems.forEach(item => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${item.name} - ${item.description} (${item.price})`;
+        menuList.appendChild(listItem);
+    });
+
+
 
 
     // adding created title and tagline to the content div
     content.appendChild(menu);
-    content.appendChild(menuOne);
-    content.appendChild(menuTwo);
+    content.appendChild(menuList);
+
 
 
 }
