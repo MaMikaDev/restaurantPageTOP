ba28db0ab95500c370945229ceeb5f21625f8d15
Martyna Mika <67931479+MaMikaDev@users.noreply.github.com>
2025-09-18 10:24:51 +0100

    added some lorem ipsum to the About section, corrected the reference errors as welll
    All works as expected, but is pretty rough looking.

--- src/about.js
+++ src/about.js
@@ -1,6 +1,16 @@
 function loadAbout(){
     console.log("loading about test");
 
+    // get the content div from the template.html
+    const content = document.getElementById("content");
+
+    const about = document.createElement("p");
+    about.textContent = "This is a very long about section, so instead of me writing lots and lots here, lets have some Lorem Ipsum instead: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu lobortis ex, quis cursus augue. Curabitur dapibus ac quam ac convallis. Aenean vehicula, tortor at ultrices euismod, lacus neque consectetur leo, sed auctor nisl libero ac enim. Fusce est augue, blandit eget tincidunt eu, consequat non elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed condimentum venenatis est, vitae volutpat justo molestie nec. In fermentum convallis nisl nec placerat. Maecenas porta augue vitae sodales dictum. Nullam bibendum magna elit, at vestibulum nulla varius nec. Pellentesque eleifend elementum mauris dignissim auctor. In quis sem sed ex pretium lobortis vitae nec massa. Aenean suscipit vitae lectus sed elementum. Sed viverra lectus vulputate nisi vulputate vulputate.";
+
+    about.style.padding = "20px";
+
+    content.appendChild(about);
+
 }
 
 export default loadAbout;
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,29 @@
 function loadMenu(){
     console.log("loading menu test");
 
+        // get the content div from the template.html
+    const content = document.getElementById("content");
+
+    // create the elements for the content
+    const menu = document.createElement("h2");
+    menu.textContent = "Our Current Menu";
+
+    // create extra div just for testing
+    const menuOne = document.createElement("p");
+    menuOne.textContent = "All the meat you can eat (and some vegetables)";
+    menuOne.style.padding = "20px";
+
+     const menuTwo = document.createElement("p");
+    menuTwo.textContent = "Marimo special (rice with fish basically)";
+    menuTwo.style.padding = "20px";
+
+
+    // adding created title and tagline to the content div
+    content.appendChild(menu);
+    content.appendChild(menuOne);
+    content.appendChild(menuTwo);
+
+
 }
 
 export default loadMenu;
