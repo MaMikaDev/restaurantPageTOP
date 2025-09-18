6e98f51749e0934242923e97eec96ea8121e4297
Martyna Mika <67931479+MaMikaDev@users.noreply.github.com>
2025-09-18 08:38:29 +0100

    appending content to div via webpack

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,5 +1,21 @@
 function pageLoad(){
 
+    // get the content div from the template.html
+    document.getElementById("content");
+
+    // create the elements for the content
+    const title = document.createElement("h1");
+    title.textContent = "Welcome to All Blue";
+
+    // create extra div just for testing
+    const tagline = document.createElement("p");
+    tagline.textContent = "Where the only thing worse than food is the service";
+    tagline.style.padding = "20px";
+
+    // adding created title and tagline to the content div
+    content.appendChild(title);
+    content.appendChild(tagline);
+
 }
 
 
