601379488edeaa23e43333cbce9338392113b3f3
Martyna Mika <67931479+MaMikaDev@users.noreply.github.com>
2025-09-18 09:30:10 +0100

    mini colour change & navigation bar correction

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import "./styles.css";
 
 pageLoad();
 
+// getting the home button
+const homeButton = document.getElementById("homeButton");
+
+homeButton.addEventListener("click", () => {
+    // ensure the div is cleaned
+    const content = document.getElementById("content");
+    content.textContent = "";
+
+    pageLoad();
+});
+
 
 console.log("Check that the basic set up works")
 
