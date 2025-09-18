89d3b30dfe8132895d18df3a658e57e7ac449eac
Martyna Mika <67931479+MaMikaDev@users.noreply.github.com>
2025-09-18 08:19:52 +0100

    Added styles.css file with a purple background
    the white was way too bright as a background

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import pageLoad from "./pageLoad.js";
+import "./styles.css";
+
+pageLoad();
+
+
 console.log("Check that the basic set up works")
+
---
+++ src/pageLoad.js
@@ -0,0 +1,6 @@
+function pageLoad(){
+
+}
+
+
+export default pageLoad;
